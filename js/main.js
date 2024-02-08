3a84cf2ba9371a0c718decefcf3329a5d2d2ba66
Matteoo000 <m.palstra@sintlucasedu.nl>
2024-02-08 10:59:12 +0100

    before break safe + first things in javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 document.getElementById("title").innerText = "point and click adventure game ";
+
+//Game window reference
+const playscreen = document.getElementById("playscreen");
+
+//Main character 
+const maincharacter = document.getElementById("maincharacter");
+
+playscreen.onclick = function (e) {
+    console.log(e.target.id);
+}
