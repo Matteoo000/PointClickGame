466339b17ba92dd3a6169d69c1ac2fbe24cb5330
Matteoo000 <m.palstra@sintlucasedu.nl>
2024-02-08 12:55:05 +0100

    end of class
    added css for player
    added tilemap
    added javascript functionality

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ const playscreen = document.getElementById("playscreen");
 const maincharacter = document.getElementById("maincharacter");
 const offsetcharacterX = 16;
 
+//foreground items
+
+const door1 = document.getElementById("door1");
+
 playscreen.onclick = function (e) {
     var rect = playscreen.getBoundingClientRect();
     var x = e.clientX - rect.left;
@@ -15,4 +19,23 @@ playscreen.onclick = function (e) {
     console.log(e.target.id);
     maincharacter.style.left = x - offsetcharacterX + "px";
     maincharacter.style.top = y - offsetcharacterX + "px";
+
+    switch (e.target.id) {
+        case "door1":
+            maincharacter.style.backgroundColor = "#FFFF00"
+            door1.style.opacity = 0.5;
+            break;
+        case "sign":
+            mainCharacter.style.backgroundColor = "#FF0000"
+            sign.style.opacity = 0.5;
+            break;
+        default:
+            //explode
+            maincharacter.style.backgroundColor = "#FFFFFF";
+            door1.style.opacity = 1;
+            sign.style.opacity = 1;
+            break;
+
+    }
+
 }
