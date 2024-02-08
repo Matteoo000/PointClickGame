75354e61604f282f25a38f3b6e673cc5078e7659
Matteoo000 <m.palstra@sintlucasedu.nl>
2024-02-08 11:55:22 +0100

    +move maincharacter to cursor location

--- js/main.js
+++ js/main.js
@@ -5,7 +5,14 @@ const playscreen = document.getElementById("playscreen");
 
 //Main character 
 const maincharacter = document.getElementById("maincharacter");
+const offsetcharacterX = 16;
 
 playscreen.onclick = function (e) {
+    var rect = playscreen.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+
     console.log(e.target.id);
+    maincharacter.style.left = x - offsetcharacterX + "px";
+    maincharacter.style.top = y - offsetcharacterX + "px";
 }
