f51da2e5d4d860bc11f73694b8b5dd67af0dda58
Matteoo000 <m.palstra@sintlucasedu.nl>
2024-02-19 10:52:22 +0100

    before break

--- js/main.js
+++ js/main.js
@@ -3,13 +3,21 @@ document.getElementById("title").innerText = "point and click adventure game ";
 //Game window reference
 const playscreen = document.getElementById("playscreen");
 
+//game state
+gameState = {
+    "door2locked": true,
+}
+
 //Main character 
 const maincharacter = document.getElementById("maincharacter");
 const offsetcharacterX = 16;
 
-//foreground items
+//inventory
+const inventorybox = document.getElementById('inventorybox');
 
+//foreground items
 const door1 = document.getElementById("door1");
+const sing = document.getElementById('sign');
 
 playscreen.onclick = function (e) {
     var rect = playscreen.getBoundingClientRect();
@@ -20,15 +28,48 @@ playscreen.onclick = function (e) {
     maincharacter.style.left = x - offsetcharacterX + "px";
     maincharacter.style.top = y - offsetcharacterX + "px";
 
+
     switch (e.target.id) {
         case "door1":
             maincharacter.style.backgroundColor = "#FFFF00"
             door1.style.opacity = 0.5;
+            sign.style.opacity = 1;
+
+            if (document.getElementById("key1") !== null) {
+                console.log('Found key');
+                document.getElementById("key1").remove();
+                const keyElement = document.createElement("Li");
+                keyElement.id = "inv-key";
+                keyElement.innerText = "key";
+                inventorylist.appendChild(keyElement);
+
+            }
             break;
+
+        case "door2":
+            if (gameState.door2locked == true) {
+                //check if we have key
+                if (document.getElementById("inv-key") !== null) {
+                    // yes unlock door
+                    gameState.door2locked = false;
+                    //gamestate lvl1door2 = "unlocked"
+                }
+                else {
+                    //no alert door locked
+                    alert("Door is locked find key for acces")
+                }
+            }
+            else {
+                console.log("Enter building");
+            }
+            break;
+
         case "sign":
             mainCharacter.style.backgroundColor = "#FF0000"
             sign.style.opacity = 0.5;
+            door1.style.opacity = 1;
             break;
+
         default:
             //explode
             maincharacter.style.backgroundColor = "#FFFFFF";
